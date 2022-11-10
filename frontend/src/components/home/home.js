2e4e03fb77901e2c00b2250a0a987746084ce074
Carolina <carolinrg@gmail.com>
2022-11-10 18:43:48 -0500

    frontend

---
+++ frontend/src/components/home/home.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState, Fragment } from "react";
+import { getProductos } from "../../api/apiProductos.js";
+import "./home.css";
+
+export function Home(){
+
+  const [productos, setProductos] = useState([])
+	useEffect(() => {
+
+		const getdata = async () => {
+
+			const data = await getProductos();
+
+			setProductos(data)
+			console.log(data)
+
+		}
+
+		getdata()
+
+
+	}, [])
+
+    return(
+        <Fragment>
+        <div class="text-center font-link mt-4">
+        <h2 class="font-main-title-home">Rápido y Seguro</h2>
+        <p class="font-main-text-home">Ven ya por tu nueva bicicleta y mucho más</p>
+        </div>
+
+        <div class="text-center mt-5 justify-content-around ">
+    
+    
+    <div className="row row-cols-1 row-cols-md-3 g-4 m-3" width={150} height={300}>
+    
+    {productos.map((item, index) => {
+
+return (
+
+  <div className="col ">
+  <div className='card'>
+  <img class="card-img-top" width={150} height={210} src={item.urlImagen} alt={item.nombre} />
+  <div class="card-body font-lnk">
+  <h5>{item.nombre}</h5>
+  <p class="card-description">{item.descripcion}</p>
+  <p>{item.precio}</p>
+  <p class="badge bg-primary text-center">Stock: {item.stock}</p>
+  <div>
+  <button class="btn btn-outline-dark">
+    More Info
+  </button>
+  </div>
+  </div> 
+</div>
+</div>
+ 
+)
+})}
+      
+    </div>
+    </div>
+
+        </Fragment>
+    )
+}
---
+++ frontend/src/data/data.js
@@ -0,0 +1,39 @@
+export const heroes = [
+    {
+        id: 1,
+        name:'Marlin 7',
+        owner:'Trek',
+        image:'./Marlin7.png',
+        price:'$ 7.500',
+        stock:'2',
+        additional:{
+            notes:'La Marlin 7 es la mejor compañera en los senderos y en tus paseos, gracias a la confianza que inspira su geometría de trail. Ofrece el equilibrio perfecto entre precio y rendimiento, con una horquilla de suspensión RockShox mejorada que te permite superar con velocidad las piedras y los baches de los caminos, y con la precisión del cambio Shimano Deore.',
+            link:'https://www.trekbikes.com/es/es_ES/bicicletas/bicicletas-de-monta%C3%B1a/bicicletas-de-monta%C3%B1a-de-cross-country/marlin/marlin-7-gen-3/p/36967/?colorCode=grey'
+        }
+    },
+
+    {
+        id: 2,
+        name:'Marlin 8',
+        owner:'Trek',
+        image:'./Marlin8.jpg',
+        price:'$ 7.500',
+        stock:'1',
+        additional:{
+            notes:'Se trata de un superhéroe que emplea sus habilidades sobrehumanas, reminiscentes de una araña, para combatir a otros supervillanos que persiguen fines siniestros.',
+            link:'https://es.wikipedia.org/wiki/Spiderman'
+        }
+    },
+     {
+        id: 3,
+        name:'Session 9',
+        owner:'Trek',
+        image:'./Session9y.png',
+        price:'$ 7.500',
+        stock:'8',
+        additional:{
+            notes:'En sus apariciones de cómic, el personaje es a la vez Hulk, un ser humanoide enorme de piel verde, corpulento y musculoso que posee una gran fortaleza física, y su alter ego el Dr. Robert Bruce Banner (o Dr. David Bruce Banner),3​4​ un físico socialmente retraído, débil físicamente y emocionalmente reservado, las dos personalidades existentes como independientes y con resentimiento de la otra.',
+            link:'https://es.wikipedia.org/wiki/Hulk'
+        }
+    },
+];
